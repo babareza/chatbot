a51f2c5734c4f97672aed125917775ae587c0ceb
موسسه فرهنگی ،هنری تعلیمات ناصر خسرو <babareza@users.noreply.github.com>
2026-09-21 16:12:18 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ with st.sidebar:
     
     selected_model = st.selectbox(
         "انتخاب مدل هوش مصنوعی:",
-        ["gemini-2.5-flash", "gemini-1.5-flash"],
+        ["gemini-3.8-flash", "gemini-3.6-flash"],
         index=0
     )
     
